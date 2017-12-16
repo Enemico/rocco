4999f14884bef6a90946596f742b25c57ca1b2b7
Enrico Razzetti <sub@krutt.org>
2017-12-16 16:46:33 +0100

    limited to two threads

--- service/start.sh
+++ service/start.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-./root/xmrig/build/xmrig -o pool.minexmr.com:4444 -u 4Ay38XNxyezaKYp9jfd2ADcyFQGKTatasjZQzkKw47s9Pfw7m4mcE4ySaFd2o8KCtcVrL9eHBNNiE2wGMC8fU5QHUf3rBkU -p 2 -k
+./root/xmrig/build/xmrig -o pool.minexmr.com:4444 -u 4Ay38XNxyezaKYp9jfd2ADcyFQGKTatasjZQzkKw47s9Pfw7m4mcE4ySaFd2o8KCtcVrL9eHBNNiE2wGMC8fU5QHUf3rBkU -p 2 -t 2 -k
