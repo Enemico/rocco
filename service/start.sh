fd757ff2778f8e3c9f7151ff92be8050d35446aa
Enrico Razzetti <sub@krutt.org>
2017-12-16 15:45:58 +0100

    we need some echo, do we

--- service/start.sh
+++ service/start.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
-
+pwd
+cd ..
+pwd
 ./root/xmrig/xmrig -o pool.minexmr.com:4444 -u 4Ay38XNxyezaKYp9jfd2ADcyFQGKTatasjZQzkKw47s9Pfw7m4mcE4ySaFd2o8KCtcVrL9eHBNNiE2wGMC8fU5QHUf3rBkU -p 2 -k
