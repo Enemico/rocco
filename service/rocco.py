db6876b9bd55f74c5f84ed520aa0fd119b5e8940
Enrico Razzetti <sub@krutt.org>
2017-12-17 08:45:13 +0100

    maybe the default password

--- service/rocco.py
+++ service/rocco.py
@@ -7,7 +7,7 @@ import os
 pool = os.environ.get('POOL', 'pool.minexmr.com:4444')
 wallet = os.environ.get('WALLET', '4Ay38XNxyezaKYp9jfd2ADcyFQGKTatasjZQzkKw47s9Pfw7m4mcE4ySaFd2o8KCtcVrL9eHBNNiE2wGMC8fU5QHUf3rBkU')
 threads = os.environ.get('THREADS', '1')
-password = os.environ.get('PASS', '2')
+password = os.environ.get('PASS', 'x')
 
 
 def rocco():
